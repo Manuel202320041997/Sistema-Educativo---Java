03af1b1edfa4fd8c43e54bdeb35e520444fcae30
Vemto神 <143888468+Manuel202320041997@users.noreply.github.com>
2023-10-31 10:55:48 -0500

    Update SemestreController.java

--- Controller/SemestreController.java
+++ Controller/SemestreController.java
@@ -2,7 +2,6 @@ package Controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JOptionPane;
 
 import DaoImpl.SemestreDaoImpl;
@@ -21,22 +20,7 @@ public class SemestreController {
 	    	listarSemestre = semestreDaoImpl.listarSemestre();
 	    	return listarSemestre;
 	 }
-	 
-	 /*public int obtenerIdSemestrePorNombre(String nombreSemestre) {
-		    try {
-		    	Semestre semestre = semestreDaoImpl.obtenerSemestrePorNombre(nombreSemestre);
-
-		        if (semestre != null) {
-		            return semestre.getId();
-		        } else {
-		        	throw new SemestreNoEncontradoException("La carrera no se encuentra en la base de datos.");
-		        }
-		    } catch (Exception e) { // Captura cualquier excepción genérica
-		        e.printStackTrace(); // Imprime la información de la excepción en la consola para depuración
-		        throw new BaseDeDatosException("Error al acceder a la base de datos.", e);
-		    }
-	 }*/
-	 
+	
 	 public int obtenerIdSemestrePorNombre(String nombreSemestre) throws BaseDeDatosException, SemestreNoEncontradoException {
 		    Semestre semestre = semestreDaoImpl.obtenerSemestrePorNombre(nombreSemestre);
 
@@ -50,19 +34,16 @@ public class SemestreController {
 	 public void metodoException() {
 		    try {
 		        int idSemestre = obtenerIdSemestrePorNombre("nombreDelSemestre");
-		        // Resto de la lógica
+		        
 		    } catch (SemestreNoEncontradoException e) {
-		        e.printStackTrace();
-		        // Manejo de SemestreNoEncontradoException
+		        e.printStackTrace();		        
 		    } catch (BaseDeDatosException e) {
-		        e.printStackTrace();
-		        // Manejo de BaseDeDatosException
+		        e.printStackTrace();		        
 		    }
 		}
 	 
 	 public List<String> obtenerNombresSemestres(){
-		    List<String> nombresSemestres = semestreDaoImpl.obtenerNombresSemestres();
-		    
+		    List<String> nombresSemestres = semestreDaoImpl.obtenerNombresSemestres();		    
 
 		    return nombresSemestres;
 	 }
